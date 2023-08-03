c502e1bbaa5a3245de1d72aa55f06e56b8f2d5e2
Sourcery AI <>
2023-08-03 07:05:31 +0000

    'Refactored by Sourcery'

--- materialize.py
+++ materialize.py
@@ -70,10 +70,8 @@ def main(stdscr, options):
         new_screen(screen, lines, cols, started_cols, options.one_prob)
         if options.verbose:
             print(f'{len(screen)} x {len(screen[0])}', file=sys.stderr)
-        line_nr = 0
-        for line in screen:
-            col_nr = 0
-            for char in line:
+        for line_nr, line in enumerate(screen):
+            for col_nr, char in enumerate(line):
                 if pict[line_nr][col_nr] == '2':
                     stdscr.addstr(line_nr, col_nr, figure_char,
                                   curses.color_pair(2) | curses.A_BOLD)
@@ -84,8 +82,6 @@ def main(stdscr, options):
                             pict[line_nr][col_nr] = '2'
                     stdscr.addstr(line_nr, col_nr, char,
                                   curses.color_pair(1))
-                col_nr += 1
-            line_nr += 1
         stdscr.refresh()
         time.sleep(options.sleep)
         update_started_cols(started_cols, options.ragged_prob)
@@ -93,8 +89,7 @@ def main(stdscr, options):
 def read_picture(file_name):
     pict = []
     with open(file_name, 'r') as pict_file:
-        for line in pict_file:
-            pict.append(list(line.strip()))
+        pict.extend(list(line.strip()) for line in pict_file)
     return pict
     
 
